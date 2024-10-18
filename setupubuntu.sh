38251a92d30fa7d7aae57d106e82e942a5b5fd11
Aditya Mitra <adityaarghya0@gmail.com>
2024-10-18 11:36:27 +0530

    Update setupubuntu.sh

--- setupubuntu.sh
+++ setupubuntu.sh
@@ -1,5 +1,9 @@
+export ZSH_DISABLE_COMPFIX=true
+export TERM=xterm-256color
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games
+export PYTHONHASHSEED=0
 apt-get update && apt-get -y upgrade
-apt-get install -y zsh python3-dev 
+apt-get install -y zsh
 apt-get install -y curl wget git
 apt-get install -y lsb-release
 apt-get install -y neofetch
