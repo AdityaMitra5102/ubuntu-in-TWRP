b2c2901d3d8280f1d12ad7eda4ae8f9ec9a622f2
Aditya Mitra <adityaarghya0@gmail.com>
2024-10-18 10:08:34 +0530

    Update setupubuntu.sh

--- setupubuntu.sh
+++ setupubuntu.sh
@@ -3,6 +3,10 @@ apt-get install -y zsh
 apt-get install -y neofetch 
 apt-get install -y lsb-release 
 apt-get install -y python3-dev 
+apt-get install -y curl
+apt-get install -y wget
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv /root/.zshrc /root/.zshrcbkp
 cat > /root/.zshrc <<-EOM
 export ZSH_DISABLE_COMPFIX=true
 export TERM=xterm-256color
