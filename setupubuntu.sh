71bc31890682c4306a4f2f75a0eada949dd3c230
Aditya Mitra <adityaarghya0@gmail.com>
2024-10-18 10:59:18 +0530

    Update setupubuntu.sh

--- setupubuntu.sh
+++ setupubuntu.sh
@@ -5,4 +5,5 @@ export ZSH_DISABLE_COMPFIX=true
 export TERM=xterm-256color
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games
 export PYTHONHASHSEED=0
+mkdir /dev
 EOM
