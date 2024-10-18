0fc558004aff5bc91af8e4411ab1b2a1641f6d4b
Aditya Mitra <adityaarghya0@gmail.com>
2024-10-18 12:08:26 +0530

    Update setupubuntu.sh

--- setupubuntu.sh
+++ setupubuntu.sh
@@ -7,7 +7,7 @@ apt-get install -y zsh
 apt-get install -y curl wget git sed
 apt-get install -y lsb-release
 apt-get install -y neofetch
-sed -i 'i1 /usr/bin/python3' /bin/lsb_release
+sed -i 'i1 #!/usr/bin/python3' /bin/lsb_release
 
 cat > /root/.zshrc <<-EOM
 export ZSH_DISABLE_COMPFIX=true
