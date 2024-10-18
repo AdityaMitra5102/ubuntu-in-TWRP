94ded67ddf25a8f6d3c7abd78a84034850961dd8
Aditya Mitra <adityaarghya0@gmail.com>
2024-10-18 10:32:20 +0530

    Update setupubuntu.sh

--- setupubuntu.sh
+++ setupubuntu.sh
@@ -1,17 +1,8 @@
 apt-get update && apt-get -y upgrade
-apt-get install -y zsh 
-apt-get install -y neofetch 
-apt-get install -y lsb-release 
-apt-get install -y python3-dev 
-apt-get install -y curl
-apt-get install -y wget
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mv /root/.zshrc /root/.zshrcbkp
+apt-get install -y zsh neofetch lsb-release python3-dev curl wget git
 cat > /root/.zshrc <<-EOM
 export ZSH_DISABLE_COMPFIX=true
 export TERM=xterm-256color
-export PARH=$PATH:/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games
 export PYTHONHASHSEED=0
 EOM
-
-neofetch
