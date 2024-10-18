c32c0f74708c419812815a35dd655f499d6b7991
Aditya Mitra <adityaarghya0@gmail.com>
2024-10-18 12:07:59 +0530

    Update setupubuntu.sh

--- setupubuntu.sh
+++ setupubuntu.sh
@@ -4,9 +4,11 @@ export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/
 export PYTHONHASHSEED=0
 apt-get update && apt-get -y upgrade
 apt-get install -y zsh
-apt-get install -y curl wget git
+apt-get install -y curl wget git sed
 apt-get install -y lsb-release
 apt-get install -y neofetch
+sed -i 'i1 /usr/bin/python3' /bin/lsb_release
+
 cat > /root/.zshrc <<-EOM
 export ZSH_DISABLE_COMPFIX=true
 export TERM=xterm-256color
@@ -14,3 +16,4 @@ export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/
 export PYTHONHASHSEED=0
 mkdir /dev
 EOM
+ 
